73e3abeef93239ab1bec6bf7d30ca0f259a4c980
Ungrim <arkarash@gmail.com>
2020-09-13 08:47:01 +0200

    Final

---
+++ Empleados/Data/MysqlDatabase.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace Empleados.Data
+{
+    public class MysqlDatabase : IDisposable
+    {
+        public MySqlConnection Connection;
+
+        public MysqlDatabase(string connectionString)
+        {
+            Connection = new MySqlConnection(connectionString);
+            this.Connection.Open();
+        }
+
+        public void Dispose()
+        {
+            Connection.Close();
+        }
+    }
+}
